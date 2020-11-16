f7db504e54cbc4195a9f6823449a7752fd5c79c4
Patrick S <psokol@uwaterloo.ca>
2020-11-16 02:47:37 -0500

    Updated dataset

--- raw_data/weatherScript.py
+++ raw_data/weatherScript.py
@@ -80,8 +80,8 @@ def extractDayAndMonth():
     data[0].append("Month")
     for i in range(1, 207649):
         date = data[i][0]
-        data[i].append(date.strftime("%A") )
-        data[i].append(date.strftime("%B") )
+        data[i].append(date.weekday())
+        data[i].append(date.strftime("%m") )
 
 def validateDate():
     allDates = []
@@ -154,7 +154,193 @@ def addIncidents():
                 date_time_obj += datetime.timedelta(minutes=5)
           #  date_time_obj += datetime.timedelta(hours=1)
                 
-            
+def buildMaxLaneClosure():
+    excelFileName = 'lane_closures_dataset.xlsx'
+    workbook = openpyxl.load_workbook(filename=excelFileName)
+    workSheet = workbook.active
+    startDateCol = 15
+    endDateCol = 18
+    closureLanesCol = 48
+    totalLanesCol = 49
+
+    maxRow = 8805
+    currStartTimeRow = 2
+    currMaxLanes = 0
+
+    currEndTimeRow = 2
+
+   # currStartTime = datetime.datetime.strptime(workSheet.cell(row=currStartTimeRow, column=startDateCol).value, '%Y-%m-%d %H:%M:%S')
+   # currStartTime = workSheet.cell(row=currStartTimeRow, column=startDateCol).value
+   # currMaxLanes = workSheet.cell(row=currStartTimeRow, column=closureLanesCol).value / workSheet.cell(row=currStartTimeRow, column=totalLanesCol).value
+    
+    currStartTimeRow += 1
+
+    #currEndTime = workSheet.cell(row=currEndTimeRow, column=endDateCol).value
+
+    #currEndTime = datetime.datetime.strptime(workSheet.cell(row=currEndTimeRow, column=endDateCol).value, '%Y-%m-%d %H:%M:%S')
+    
+    '''
+    maxLaneClosuresStartTime = []
+
+    while currStartTimeRow <= maxRow:
+        tempStartTime = workSheet.cell(row=currStartTimeRow, column=startDateCol).value
+       # tempStartTime= datetime.datetime.strptime(workSheet.cell(row=currStartTimeRow, column=startDateCol).value, '%Y-%m-%d %H:%M:%S')
+        tempMax = workSheet.cell(row=currStartTimeRow, column=closureLanesCol).value / workSheet.cell(row=currStartTimeRow, column=totalLanesCol).value
+      #  print('Comparing ', tempStartTime, ' with ', currStartTime)
+      #  print('Result: ', tempStartTime > currStartTime )
+      #  print('Curr max lanes: ', currMaxLanes)
+        if tempStartTime > currStartTime:
+            maxLaneClosuresStartTime.append((currStartTime, currMaxLanes))
+            currMaxLanes = tempMax
+            currStartTime = tempStartTime
+        else: 
+            currMaxLanes =  max(currMaxLanes, tempMax)
+          #  print(currStartTime, ' ', maxRow)
+            if currStartTimeRow == maxRow:
+                maxLaneClosuresStartTime.append((tempStartTime, currMaxLanes))
+        currStartTimeRow += 1
+    '''
+
+  #  print('#########################################################################')
+    #print ('Here')
+  #  for i in range(len(maxLaneClosuresStartTime)):
+  #      print(maxLaneClosuresStartTime[i])
+
+   # print('#########################################################################')
+
+    currMaxLanes = 0
+
+   # currEndTimeRow = 2
+
+    #currEndTime = workSheet.cell(row=currEndTimeRow, column=endDateCol).value
+   
+   # currEndTimeRow += 1
+    startTimeList = []   
+
+
+    currStartTimeRow = 2
+    while currStartTimeRow <= maxRow:
+        #startTime = datetime.datetime.strptime(workSheet.cell(row=currStartTimeRow, column=startDateCol).value, '%Y-%m-%d %H:%M:%S')
+        startTime = workSheet.cell(row=currStartTimeRow, column=startDateCol).value
+        lanesClosed = workSheet.cell(row=currStartTimeRow, column=closureLanesCol).value
+        totalLanes = workSheet.cell(row=currStartTimeRow, column=totalLanesCol).value
+        if lanesClosed != 0 and totalLanes >= 3:
+            lanesClosedPercent = lanesClosed / totalLanes
+            startTimeList.append((startTime, lanesClosedPercent))
+
+        currStartTimeRow += 1
+
+    startTimeList.sort(key=lambda tup: tup[0])  # sorts in place
+
+   # currEndTime = workSheet.cell(row=currEndTimeRow, column=endDateCol).value
+    endTimeList = []   
+
+
+    currEndTimeRow = 2
+    while currEndTimeRow <= maxRow:
+        endTime = datetime.datetime.strptime(workSheet.cell(row=currEndTimeRow, column=endDateCol).value, '%Y-%m-%d %H:%M:%S')
+        lanesClosed = workSheet.cell(row=currEndTimeRow, column=closureLanesCol).value 
+        totalLanes = workSheet.cell(row=currEndTimeRow, column=totalLanesCol).value
+        if lanesClosed != 0 and totalLanes >= 3:
+            lanesClosedPercent = lanesClosed / totalLanes
+            endTimeList.append((endTime, -1 * lanesClosedPercent))
+        currEndTimeRow += 1
+
+    endTimeList.sort(key=lambda tup: tup[0])  # sorts in place
+
+  #  print('Edge case check: ', endTimeList[len(endTimeList) - 1])
+
+
+
+   # maxLaneClosuresEndTime.sort(key=lambda tup: tup[0])  # sorts in place
+    
+
+
+    lengthEndTime = len(endTimeList)
+    lengthStartTime = len(startTimeList)
+
+    currStartTime = startTimeList[0][0]
+    currEndTime = endTimeList[0][0]
+
+    startTimeCounter = 1
+    endTimeCounter = 1
+    
+    result = []
+
+    for temp in startTimeList:
+        result.append(temp)
+    for temp in endTimeList:
+        result.append(temp)
+        
+    result.sort(key=lambda tup: tup[0])
+
+    #print('LENGTH OF ENDTIME: ', len(endTimeList))
+    #print('LENGTH OF STARTTIME: ', len(startTimeList))
+   # print('LENGTH OF RESULT: ', len(result))
+   # for i in range(len(result)):
+  #      print(result[i][0].strftime("%m/%d/%Y, %H:%M:%S"), ' ' , result[i][1])
+  #  print('#########################################################################')
+
+    data[0].append('LaneClosures')
+
+   # lanesClosed = len(result)
+    lanesClosedCounter = 0
+  #  currLanesClosed = 0
+    lastRowIndex = 207649
+
+    stack = []
+    stack.append(0)
+    for i in range(1, lastRowIndex):
+        date = data[i][0]
+      #  print('Date checking: ', date)
+       # print(result[lanesClosedCounter + 1][0].strftime("%m/%d/%Y, %H:%M:%S"), ' ' , result[lanesClosedCounter + 1][1])
+       # print('stack is: ', stack)
+        
+
+#index < len
+
+        if lanesClosedCounter + 1 >= len(result):
+            stack = [0]
+
+        elif date >= result[lanesClosedCounter][0]:
+         #   print(result[lanesClosedCounter][0].strftime("%m/%d/%Y, %H:%M:%S"), ' ' , result[lanesClosedCounter][1])
+            lanesClosedCounter = updateSet(lanesClosedCounter, stack, result, date)
+
+
+
+        data[i].append(max(stack))    
+
+
+    #    data[i].append(currLanesClosed)
+
+
+
+   # for i in range(len(maxLaneClosures)):
+       # print(maxLaneClosures[i])
+
+
+
+def updateSet(lanesClosedCounter, stack, result, date):
+    while lanesClosedCounter < len(result) and (date >= result[lanesClosedCounter][0]):
+     #   if date >= result[lanesClosedCounter + 1][0]:
+        #    lanesClosedCounter += 1
+
+        tempLanesClosed = result[lanesClosedCounter][1]
+        # print('Here: ' , tempLanesClosed)
+        if tempLanesClosed < 0:
+          #  if (-1*tempLanesClosed) in stack:
+            stack.remove(-1*tempLanesClosed)
+        elif tempLanesClosed > 0 :
+            stack.append(tempLanesClosed)
+        lanesClosedCounter += 1
+    return lanesClosedCounter
+
+def convertToEpoch():
+    for i in range(1, len(data)):
+        if (data[i][0] != None):
+            data[i][0] = data[i][0].timestamp()
+
+
 def addWeather():
     lastRowIndex = 207649
    # print(lastRowIndex)
@@ -206,6 +392,9 @@ def addWeather():
             indexOfDate += 1
           #  date_time_obj += datetime.timedelta(hours=1)
 
+#def trimDataSet(startTime, endTime):
+#    while
+
 def addWeatherToDataSet(sheet):
 
     print('Getting date column')
@@ -232,6 +421,8 @@ def addWeatherToDataSet(sheet):
     addHolidays()
     addIncidents()
     addWeather()
+    buildMaxLaneClosure()
+    convertToEpoch()
     datasetWB.close()
     '''
   #  date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
@@ -295,3 +486,4 @@ def addWeatherToDataSet(sheet):
 
 
 addWeatherToDataSet(datasetSheet)
+#buildMaxLaneClosure()
