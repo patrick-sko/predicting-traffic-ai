5b4873327c936fea1ed58bccc0e81a67d051c46f
jrsumaba <jrsumaba@student.cs.uwaterloo.ca>
2020-11-17 15:10:12 -0500

    Change to root mean squared error

--- src/train_linear.py
+++ src/train_linear.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.metrics import mean_squared_error
 
 import linear_regression as lr
 from data_manip import sample
@@ -26,8 +25,9 @@ EXPLANATORY_COLS = ["Date", "TOD","Day","Month","Holiday","Incidents","humidity"
 individual_ft_regs = lr.train_regress_individual(training, EXPLANATORY_COLS, RESPONSE_COL)
 individual_validation_errors = lr.predict_regress_individual(individual_ft_regs, validation, RESPONSE_COL)
 
+print("Individual feature RMSEs:")
 for k,v in individual_validation_errors.items():
-    print("validation Error of feature " + k + ": " + str(v))
+    print("validation RMSE of feature " + k + ": " + str(v))
 
 # Plot a bar chart of individual error
 plt.suptitle("Mean squared errors of individual features")
@@ -35,49 +35,51 @@ plt.figure(figsize = (22,10))
 plt.bar(range(len(individual_validation_errors)), list(individual_validation_errors.values()), align='center')
 plt.xticks(range(len(individual_validation_errors)), list(individual_validation_errors.keys()), fontsize=10, wrap=True)
 plt.xlabel("feature")
-plt.ylabel("mean squared error")
+plt.ylabel("root mean squared error")
 plt.savefig(COMPARISON_PLOT_PATH + "linear_regression_individual_bar.png")
 
+print("==================================================================")
+print("Feature subset RMSEs:")
 subset_validation_errors = {}
 # An feature subset based on what intuitively affects the traffic
 EXPLANATORY_SUBSET_INTUITION = ["Date", "TOD","Day","Month","Holiday","Incidents","temperature","weather_description","LaneClosures"]
 reg_intuitive = lr.train_basic_regress_feature_set(training, EXPLANATORY_SUBSET_INTUITION, RESPONSE_COL)
 err_intuitive = lr.predict_regress_feature_set(reg_intuitive,training, EXPLANATORY_SUBSET_INTUITION , RESPONSE_COL)
 subset_validation_errors["inuitivive"] = err_intuitive
-print("intuitive features linear regression error:" + str(err_intuitive))
+print("intuitive features linear regression RMSE: " + str(err_intuitive))
 
 # An feature subset based on features that had low mean squared error
 EXPLANATORY_SUBSET_LOW_ERROR_FEW = ["Date", "humidity", "wind_direction","wind_speed","LaneClosures"]
 reg_low_error_few = lr.train_basic_regress_feature_set(training, EXPLANATORY_SUBSET_LOW_ERROR_FEW , RESPONSE_COL)
 err_low_few = lr.predict_regress_feature_set(reg_low_error_few, training, EXPLANATORY_SUBSET_LOW_ERROR_FEW, RESPONSE_COL)
 subset_validation_errors["lowest error"] = err_low_few
-print("low error features linear regression error:" + str(err_low_few))
+print("low error features linear regression RMSE: " + str(err_low_few))
 
 # Forecastable features (that can be predicted)
 EXPLANATORY_SUBSET_PREDICTABLE = ["Date", "TOD","Day","Month","Holiday","humidity","pressure","temperature","weather_description","wind_direction","wind_speed","LaneClosures"]
 reg_predictable = lr.train_basic_regress_feature_set(training, EXPLANATORY_SUBSET_PREDICTABLE, RESPONSE_COL)
 err_predictable = lr.predict_regress_feature_set(reg_predictable, training, EXPLANATORY_SUBSET_PREDICTABLE, RESPONSE_COL)
 subset_validation_errors["forecastable features"] = err_predictable
-print("forecastable features linear regression error:" + str(err_predictable))
+print("forecastable features linear regression RMSE: " + str(err_predictable))
 
 # Date only features
 EXPLANATORY_SUBSET_DATE_ONLY = ["Date", "TOD","Day","Month","Holiday"]
 reg_date = lr.train_basic_regress_feature_set(training, EXPLANATORY_SUBSET_DATE_ONLY, RESPONSE_COL)
 err_date = lr.predict_regress_feature_set(reg_date, training, EXPLANATORY_SUBSET_DATE_ONLY, RESPONSE_COL)
 subset_validation_errors["date only"] = err_date
-print("date only features linear regression error:" + str(err_date))
+print("date only features linear regression RMSE: " + str(err_date))
 
 # Training on all features
 reg_complete = lr.train_basic_regress_feature_set(training, EXPLANATORY_COLS, RESPONSE_COL)
 err_complete = lr.predict_regress_feature_set(reg_complete, training, EXPLANATORY_COLS, RESPONSE_COL)
 subset_validation_errors["full"] = err_complete
-print("all features linear regression error:" + str(err_complete))
+print("all features linear regression RMSE: " + str(err_complete))
 
 # plot feature subset errors in bar graph
-plt.suptitle("Mean squared errors of feature subsets")
+plt.suptitle("Root Mean squared errors of feature subsets")
 plt.figure(figsize = (12,5))
 plt.bar(range(len(subset_validation_errors)), list(subset_validation_errors.values()), align='center')
 plt.xticks(range(len(subset_validation_errors)), list(subset_validation_errors.keys()), fontsize=10)
 plt.xlabel("feature subset")
-plt.ylabel("mean squared error")
+plt.ylabel("root mean squared error")
 plt.savefig(COMPARISON_PLOT_PATH + "linear_regression_subset_bar.png")
